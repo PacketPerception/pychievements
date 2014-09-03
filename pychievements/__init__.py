2dff7c2649b601211b4b78b56aff9d39423ea54f
Brian <brian@packetperception.org>
2014-09-02 22:18:38 -0400

    Updated version for pypi

--- pychievements/__init__.py
+++ pychievements/__init__.py
@@ -16,8 +16,8 @@ __all__ = ['tracker', 'Achievement']
 
 
 __title__ = 'pychievements'
-__version__ = '0.1.1'
-__build__ = 0x000101
+__version__ = '0.1.2'
+__build__ = 0x000102
 __auther__ = 'Brian Knobbs'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2014 Brian Knobbs'
